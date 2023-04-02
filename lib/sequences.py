84aa9141046fea5064aa739091ff9f676a3df0ea
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-02 08:56:29 -0400

    python lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    array = []
+    if length > 0:
+        array.append(0)
+        if length > 1:
+            array.append(1)
+            if length > 2:
+                for i in range(2,length):
+                    array.append(array[i-1] + array[i-2])
+
+
+    
+    
+    return array
+
+print(print_fibonacci(9))
